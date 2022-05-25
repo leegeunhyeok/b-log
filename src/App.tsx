408d864c7b48974d1ea50d8b613fc2bcd10679ac
leegeunhyeok <dev.ghlee@gmail.com>
2022-05-26 03:27:12 +0900

    feat: container 스타일 정의
    - 화면 전체 크기를 사용하도록 width, height 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
+import { GitHubModule } from './modules/github';
 
 const App = () => {
   return (
-    <Container />
+    <Container>
+      <button onClick={() => {
+        GitHubModule.login();
+      }}>test</button>
+    </Container>
   );
 };
 
-const Container = styled('div')``;
+const Container = styled('div')`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
 
 export default App;
